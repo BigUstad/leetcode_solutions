e8687d2b7164bba5041d7dfee46680255482e387
Priyank Raj <25353498+BigUstad@users.noreply.github.com>
2019-09-08 08:00:01 +0000

    [LeetCode Sync] Runtime - 1032 ms (5.03%), Memory - 264.4 MB (5.20%)

---
+++ solutions/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if ( !s.length() )
+            return 0;
+        size_t runner = 1;
+        size_t follower  = 0;
+        size_t tracker = 0;
+        int foundLth = 1;
+        std::set<char> chrSet;
+        // O(n2). Not good
+        chrSet.insert(s[follower]);
+        while ( follower < (s.length() - 1) &&
+                runner < s.length() )
+        {
+            if ( chrSet.find(s[runner]) == chrSet.end() )
+            {
+                // Unique char
+                //std::cout << " Inserting: " << s[runner] << std::endl;
+                chrSet.insert(s[runner]);
+                ++runner;                
+                foundLth = std::max( foundLth, (int)chrSet.size() );
+            }
+            else
+            {
+                // This follower index doesn't cut it, so let's do the next one..
+                //std::cout << " Length: " << foundLth << " " << chrSet.size() << std::endl;
+                //std::cout << " Clear & start again." << std::endl;
+                chrSet.clear();
+                follower++;
+                chrSet.insert(s[follower]);
+                runner = follower + 1;
+            }
+        }
+        chrSet.clear();
+        return foundLth;
+    }
+};
