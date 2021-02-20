3f2ff28e72f07b6c300d125f3060882b507ccfd9
Priyank Raj <25353498+BigUstad@users.noreply.github.com>
2021-02-20 02:01:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.1 MB (100.00%)

--- solutions/0020-valid-parentheses/solution.cpp
+++ solutions/0020-valid-parentheses/solution.cpp
@@ -1,72 +1,56 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        size_t sLth = s.length();
-        // std::cout << " Length " << sLth << std::endl;
-        if ( sLth == 0 )
-            return true;
-        if ( sLth == 1)
-            return false;
-        std::string validChars("[]{}()");
-        bool lRet = true;
-        for( int i =0 ; (i < sLth && lRet); ++i )
+    bool isValid(string str) {
+        // Input sanity checks.
+        if(str.length() == 0)
         {
-            char curS = s.at(i);
-
-            if (curS == '(' || curS == '[' || curS == '{' )
+            return true;
+        }
+        if((str.length() % 2) != 0)
+        {
+            return false;
+        }
+        // Sanity check, if the input string contains any other char than the below six.
+        // Return false.
+        stack<char> cmp;
+        char open_param = '(';
+        char open_brack = '{';
+        char open_sq = '[';
+        char close_param = ')';
+        char close_brack = '}';
+        char close_sq = ']';
+        for (size_t i=0; i < str.length(); i++)
+        {
+            if (str.at(i) == open_param ||
+                str.at(i) == open_brack ||
+                str.at(i) == open_sq)
             {
-                // std::cout << " Pushing at " << i << " " << curS << std::endl;
-                cStack.push(s.at(i));
+                cmp.push(str.at(i));
             }
-            else if ( (curS == ')' || curS == ']' ||curS == '}')
-                      && !cStack.empty() )
+            if (str.at(i) == close_param ||
+                str.at(i) == close_brack ||
+                str.at(i) == close_sq)
             {
-                char stckTop = cStack.top();
-                // std::cout << " Popping at " << i << " " << stckTop << " for " << curS << std::endl;
-                switch(stckTop)
+                if (cmp.empty())
+                    return false;
+                char topOfStack = cmp.top();
+                if (topOfStack == open_param && str.at(i) != close_param)
                 {
-                    case '(':
-                        {
-                            if( curS != ')' )
-                            {
-                                lRet = false;
-                            }
-                            break;
-                        }
-                    case '[':
-                        {
-                            if( curS != ']' )
-                            {
-                                lRet = false;
-                            }
-                            break;
-                        }
-                    case '{':
-                        {
-                            if( curS != '}' )
-                            {
-                                lRet = false;
-                            }
-                            break;
-                        }
-                    default:
-                        lRet = false;
+                    return false;
                 }
-                cStack.pop();
-
-            }
-            else if ( (curS == ')' || curS == ']' ||curS == '}')
-                      && cStack.empty() )
-            {
-                // std::cout << " AT " << i << " " << curS << " without corresponding one in stack." << std::endl;
-                lRet = false;
+                if (topOfStack == open_sq && str.at(i) != close_sq)
+                {
+                    return false;
+                }
+                if (topOfStack == open_brack && str.at(i) != close_brack)
+                {
+                    return false;
+                }
+                cmp.pop();
             }
         }
-        if ( !cStack.empty() )
-            lRet = false;
-        return lRet;
-        
+        if (!cmp.empty())
+            return false;
+        return true;        
     }
-private:
-    std::stack<char> cStack;
 };
