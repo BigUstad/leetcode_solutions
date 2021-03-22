53fec38e5ee56659d7fa4ae7280d45844bd3c238
Priyank Raj <25353498+BigUstad@users.noreply.github.com>
2021-03-22 03:59:24 +0000

    [LeetCode Sync] Runtime - 8 ms (5.33%), Memory - 13.7 MB (100.00%)

--- solutions/0026-remove-duplicates-from-sorted-array/solution.cpp
+++ solutions/0026-remove-duplicates-from-sorted-array/solution.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if ( nums.size() == 0 || nums.size() == 1 )
+        if (nums.size() == 0 || nums.size() == 1)
         {
             return nums.size();
         }
-        int runner = 1;
-        int follower = 0;
-        for(; runner < nums.size();
-                ++runner )
+        int i=0, j=1;
+        while (j < nums.size())
         {
-			if ( nums[runner] != nums[follower] )
+            if (nums[j] != nums[i])
             {
-                follower++;
-                nums[follower] = nums[runner];
+                i++;
+                nums[i] = nums[j];
             }
+            j++;
         }
-        // std::cout << "Delete " << follower << " till end:" << nums.size()-1 << std::endl;
-        vector<int>::iterator followEle = nums.begin() + follower;
-        vector<int>::iterator end = nums.end();
-        //nums.erase(followEle, end);
-        //nums.erase(followEle);
-        return follower+1;
+        return i+1;
     }
 };
