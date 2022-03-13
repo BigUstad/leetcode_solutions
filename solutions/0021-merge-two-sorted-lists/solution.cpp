0ec2c4cbc093aaa8fb7ed5038b3c43d5b723d44f
Priyank Raj <25353498+BigUstad@users.noreply.github.com>
2022-03-13 08:39:27 +0000

    [LeetCode Sync] Runtime - 19 ms (0.50%), Memory - 14.8 MB (100.00%)

--- solutions/0021-merge-two-sorted-lists/solution.cpp
+++ solutions/0021-merge-two-sorted-lists/solution.cpp
@@ -3,52 +3,35 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-      if ( !l1 )
-        return l2;
-      if ( !l2 )
-        return l1;
-      ListNode* newHead = new ListNode(INT_MIN);
-      ListNode* dummyhead = newHead;
-      ListNode* prev = dummyhead;
-      ListNode* cur = nullptr;
-      ListNode* cur1 = l1;
-      ListNode* cur2 = l2;
-        
-
-        while ( cur1 != nullptr && cur2 != nullptr) //cur1 && cur2
-        {
-            if ( cur1->val <= cur2->val)
-            {
-              cur = cur1;
-              cur1 = cur1->next;
-            } 
-            else if (cur1->val > cur2->val) 
-            {
-              cur = cur2;
-              cur2 = cur2->next;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (list1 == nullptr && list2 == nullptr)
+            return nullptr;
+        if (list1 == nullptr) return list2;
+        if (list2 == nullptr) return list1;
+        auto cur1 = list1; auto cur2 = list2; auto newHead = new ListNode();
+        ListNode* prev = newHead;
+        while (cur1 != nullptr && cur2 != nullptr) {
+            if (cur1->val <= cur2->val) {
+                 prev->next = cur1;
+                cur1 = cur1->next;
+            } else {
+                 prev->next = cur2;
+                cur2 = cur2->next;
             }
-
-            if ( prev )
-              prev->next = cur;
-            prev = cur;
+            prev = prev->next;
         }
-        if ( cur1 != nullptr && prev )
-        {
+        if (cur1)
             prev->next = cur1;
-        }
-        else if ( cur2 != nullptr && prev )
-        {
+        if (cur2)
             prev->next = cur2;
-        }
-        ListNode* temp = dummyhead->next;
-        delete dummyhead;
-        return temp;
-        
+            
+        return newHead->next;
     }
 };
