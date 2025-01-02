667049ab7a0004f883265fa68d33a434ce0d2dd0
Priyank Raj <25353498+BigUstad@users.noreply.github.com>
2025-01-02 03:15:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.8 MB (89.58%)

--- solutions/0094-binary-tree-inorder-traversal/solution.cpp
+++ solutions/0094-binary-tree-inorder-traversal/solution.cpp
@@ -11,36 +11,41 @@
  */
 class Solution {
 public:
-    void inOrderHelper(TreeNode* root, vector<int>& nums)
-    {
-        if (root == nullptr)
-            return;
-        if (root->left == nullptr && root->right == nullptr)
-        {
-            nums.push_back(root->val);
-            return;
-        }
-        stack<TreeNode*> s;
-        TreeNode* cur = root;
-        while (!s.empty() || cur != nullptr)
-        {
-            //Left
-            while(cur != nullptr)
-            {
-                s.push(cur);
-                cur = cur->left;
+    void morrisTraversalHelper(TreeNode* root, std::vector<int>& result) {
+        TreeNode* curr = root;
+        while (curr != nullptr) {
+            if (curr->left == nullptr) {
+                // If no left child visit this node & go right
+                result.push_back(curr->val);
+                curr = curr->right;
+            } else {
+                // Find the inorder predecessor of curr
+                TreeNode* prev = curr->left;
+                while (prev->right != nullptr &&
+                    prev->right != curr) {
+                    prev = prev->right;
+                }
+                // Make curr the right child of its inorder predecessor
+                if (prev->right == nullptr) {
+                    prev->right = curr;
+                    curr = curr->left;
+                } else {
+                    // Revert the changes made in tree structure.
+                    prev->right = nullptr;
+                    result.push_back(curr->val);
+                    curr = curr->right;
+                }
             }
-            // Process
-            cur = s.top();
-            s.pop();
-            nums.push_back(cur->val);
-            //Right
-            cur = cur->right;
         }
     }
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> result;
-        inOrderHelper(root, result);
-        return result;
+        std::vector<int> nums;
+        if (root == nullptr) return nums;
+        if (root->left == nullptr && root->right == nullptr) {
+            nums.push_back(root->val);
+            return nums;
+        }
+        morrisTraversalHelper(root, nums);
+        return nums;
     }
 };
