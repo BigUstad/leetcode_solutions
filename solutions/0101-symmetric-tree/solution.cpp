8ec6e1db493fd9fa167d8b1e74d88bcc25c48594
Priyank Raj <25353498+BigUstad@users.noreply.github.com>
2025-01-02 06:18:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.6 MB (12.73%)

--- solutions/0101-symmetric-tree/solution.cpp
+++ solutions/0101-symmetric-tree/solution.cpp
@@ -4,55 +4,50 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-        bool lRet = true;
-        std::queue<TreeNode*> q;
-        q.push(root);
-        q.push(root);
-        while ( !q.empty() )
-        {
-            TreeNode* n1 = q.front();
-            q.pop();
-            TreeNode* n2 = q.front();
-            q.pop();
-            if ( n1 == nullptr && n2 == nullptr )
-            {
-                //std::cout << " Reached the end." << std::endl;
-                lRet &= true;
-            }
-            if ( ( n1 == nullptr && n2 != nullptr) 
-                || ( n2 == nullptr && n1 != nullptr) )
-            {
-                //std::cout << "Null node not expected: " << n1 << " " << n2 << std::endl;
-                lRet &= false;
-                break;
-            }
-            if ( n1 != nullptr && n2 != nullptr && (n1->val != n2->val) )
-            {
-                //std::cout << " Not equal: " << n1->val << " " << n2->val << std::endl;
-                lRet &= false;
-                break;
-            }
-            else
-            {
-                lRet &= true;
-            }
-
-            if (n1 != nullptr )
-                q.push(n1->left);
-            if (n2 != nullptr )
-                q.push(n2->right);
-            if (n1 != nullptr )
-                q.push(n1->right);
-            if (n2 != nullptr )
-                q.push(n2->left);
+        if (root == nullptr) {
+            return true;
         }
-        //std::cout << " Q is empty. " << std::endl;
-        return lRet;
+        if (root->left == nullptr && root->right == nullptr) {
+            return true;
+        }
+        if (root->left == nullptr || root->right == nullptr) {
+            return false;
+        }
+        std::queue<TreeNode*> q;
+        q.push(root->left);
+        q.push(root->right);
+        while (!q.empty()) {
+            TreeNode* lfront = q.front();
+            q.pop();
+            TreeNode* rfront = q.front();
+            q.pop();
+            if (lfront == nullptr && rfront == nullptr) {
+                // Check is good
+                continue;
+            }
+            if (lfront == nullptr || rfront == nullptr) {
+                // Both null ok.
+                // Either null - not symmetric
+                return false;
+            }
+            if (lfront->val != rfront->val) {
+                return false;
+            }
+            // For push, This direction --><--
+            q.push(lfront->left);
+            q.push(rfront->right);
+            q.push(lfront->right);
+            q.push(rfront->left);
+        }
+        return true;
     }
 };
