9af113308228ad8c529bcc9ab143e0cfb7738940
Priyank Raj <25353498+BigUstad@users.noreply.github.com>
2025-01-04 18:34:29 +0000

    [LeetCode Sync] Runtime - 2 ms (40.59%), Memory - 9.7 MB (37.62%)

--- solutions/1404-print-immutable-linked-list-in-reverse/solution.cpp
+++ solutions/1404-print-immutable-linked-list-in-reverse/solution.cpp
@@ -10,21 +10,81 @@
 
 class Solution {
 public:
+    // Trying O(1)
+    void print(ImmutableListNode* head, ImmutableListNode* last) {
+        if (!head || (head == last)) {
+            // No need to print print
+            return;
+        }
+        if (head->getNext() == last) {
+            // Reached end of the list
+            // Print this one.
+            head->printValue();
+            return;
+        }
+        ImmutableListNode* follower = head;
+        ImmutableListNode* runner = head;
+        // Get follower to the mid of list
+        // We only need the follower to be at the middle of the list.
+        // Runner can be nullptr
+        while (runner != last && runner->getNext() != last) {
+            follower = follower->getNext();
+            runner = runner->getNext()->getNext();
+        }
+        print(follower, last); // latter portion first
+        print(head, follower);
+    }
     void printLinkedListInReverse(ImmutableListNode* head) {
-        std::stack<ImmutableListNode*> s;
+        if (!head)
+            return;
+        if (!head->getNext()) {
+            head->printValue();
+            return;
+        }
+        // Get the last to halve the list.
+        /* ImmutableListNode* last = head;
+        while (last->getNext()) {
+            last = last->getNext();
+        } */
+        print(head, /* last */nullptr);
+    }
+    /* void printLinkedListInReverse(ImmutableListNode* head) {
+        if (!head)
+            return;
+        if (!head->getNext()) {
+            head->printValue();
+            return;
+        }
+        ImmutableListNode* runner = head;
+        ImmutableListNode* follower = head;
+        uint16_t count = 1; // head already counted
+        while (runner->getNext()) {
+            ++count;
+            runner = runner->getNext();
+        }
+        // runner is at last node.
+        while(count) {
+            runner->printValue();
+            while (follower->getNext() && follower->getNext() != runner) {
+                follower = follower->getNext();
+            }
+            runner = follower;
+            follower = head;
+            --count;
+        }
+    }*/
+    /* void printLinkedListInReverse(ImmutableListNode* head) {
+        std::stack<ImmutableListNode*, std::vector<ImmutableListNode*>> s;
         ImmutableListNode* cur = head;
         while ( cur != nullptr ) {
             s.push(cur);
             cur = cur->getNext();
         }
-        //std::cout << "[";
         while ( !s.empty() ) {
             auto e = s.top();
             e->printValue();
-            /*if ( s.size() > 1 )
-                std::cout << ",";*/
             s.pop();
         }
         //std::cout << "]";
-    }
+    } */
 };
