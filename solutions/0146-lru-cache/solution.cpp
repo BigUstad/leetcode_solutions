f6113b6985493a6b018989e327a8b67119e41240
Priyank Raj <25353498+BigUstad@users.noreply.github.com>
2025-02-10 01:56:04 +0000

    [LeetCode Sync] Runtime - 114 ms (17.82%), Memory - 182.1 MB (38.52%)

---
+++ solutions/0146-lru-cache/solution.cpp
@@ -0,0 +1,52 @@
+class LRUCache {
+public:
+    LRUCache(int capacity) {
+        count = (size_t) capacity;
+    }
+    
+    // get key makes key the most recently used key
+    int get(int key) {
+        auto c_itr = m_cache.find(key);
+        if (c_itr == m_cache.end()) {
+            return -1;
+        }
+        int l_ret = c_itr->second->second;
+        m_lru.erase(c_itr->second);
+        m_lru.push_front(std::make_pair(key, l_ret));
+        c_itr->second = m_lru.begin();
+        return l_ret;
+    }
+    
+    // put key, makes key the most recently used key
+    void put(int key, int value) {
+        auto c_itr = m_cache.find(key);
+        m_lru.push_front(std::make_pair(key, value));
+        if (c_itr != m_cache.end()) {
+            m_lru.erase(c_itr->second);
+        }
+        m_cache[key] = m_lru.begin();
+        // Delete the stale node
+        if (m_cache.size() > count) {
+            auto l_itr = m_lru.back();
+            // remove in cache
+            c_itr = m_cache.find(l_itr.first);
+            if (c_itr != m_cache.end()) { // Sanity check
+                m_cache.erase(c_itr);
+            }
+            m_lru.pop_back();
+        }
+    }
+private:
+    size_t count;
+    std::unordered_map<int, std::list<std::pair<int, int>>::iterator> m_cache;
+    // Value of most recently used key in the front
+    // Value of the stale key in the back.
+    std::list<std::pair<int, int>> m_lru;
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
