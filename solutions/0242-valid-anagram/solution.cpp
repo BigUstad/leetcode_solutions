16f316aeda508d663a14fdf6919b84302d38f957
Priyank Raj <25353498+BigUstad@users.noreply.github.com>
2025-06-14 21:29:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.7 MB (49.95%)

--- solutions/0242-valid-anagram/solution.cpp
+++ solutions/0242-valid-anagram/solution.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int totalXor = 0;
-        vector<char> cv(26,0);
-        vector<char> cv2(26,0);
-        bool lRet = true;
-        for ( char c: s )
-        {
-            totalXor ^= c;
-            cv[c-'a']++;
+        if (s.length() != t.length()) {
+            return false;
         }
-        for ( char c: t )
-        {
-            totalXor ^= c;
-            cv2[c-'a']++;
+        std::vector<short> v(26, 0);
+        for (auto c: s) {
+            v[c - 'a']++;
         }
-        if ( totalXor != 0 )
-            return false;// No need to perform further check. Increase performance.
-        for ( int i = 0; i < 26; i++ )
-        {
-            if (cv2[i] != cv[i] )
+        for (auto c: t) {
+            v[c - 'a']--;
+        }
+        for (auto i: v) {
+            if (i > 0) {
                 return false;
+            }
         }
         return true;
     }
