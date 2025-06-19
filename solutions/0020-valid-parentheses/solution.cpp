f50698d0f8fb31aa914019c60fc7e6e07992ae3d
Priyank Raj <25353498+BigUstad@users.noreply.github.com>
2025-06-19 02:13:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.5 MB (99.84%)

--- solutions/0020-valid-parentheses/solution.cpp
+++ solutions/0020-valid-parentheses/solution.cpp
@@ -2,23 +2,18 @@ class Solution {
 public:
     bool isValid(string str) {
         // Input sanity checks.
-        if(str.length() == 0)
-        {
+        if(str.length() == 0) {
             return true;
         }
-        if((str.length() % 2) != 0)
-        {
+        if((str.length() % 2) != 0) {
             return false;
         }
         // Sanity check, if the input string contains any other char than the below six.
         // Return false.
-        stack<char> cmp;
-        char open_param = '(';
-        char open_brack = '{';
-        char open_sq = '[';
-        char close_param = ')';
-        char close_brack = '}';
-        char close_sq = ']';
+        std::stack<char> cmp;
+        char open_param = '('; char close_param = ')';
+        char open_brack = '{'; char close_brack = '}';
+        char open_sq = '['; char close_sq = ']';
         for (size_t i=0; i < str.length(); i++)
         {
             if (str.at(i) == open_param ||
@@ -26,31 +21,23 @@ public:
                 str.at(i) == open_sq)
             {
                 cmp.push(str.at(i));
+                continue;
             }
-            if (str.at(i) == close_param ||
-                str.at(i) == close_brack ||
-                str.at(i) == close_sq)
-            {
-                if (cmp.empty())
-                    return false;
-                char topOfStack = cmp.top();
-                if (topOfStack == open_param && str.at(i) != close_param)
-                {
-                    return false;
-                }
-                if (topOfStack == open_sq && str.at(i) != close_sq)
-                {
-                    return false;
-                }
-                if (topOfStack == open_brack && str.at(i) != close_brack)
-                {
-                    return false;
-                }
-                cmp.pop();
+            if (cmp.empty()) {
+                return false;
             }
+            char top = cmp.top();
+            bool check_param_fail =  (top == open_param && str.at(i) != close_param) ||
+                                (top == open_sq && str.at(i) != close_sq) ||
+                                (top == open_brack && str.at(i) != close_brack);
+            if (check_param_fail) {
+                return false;
+            }
+            cmp.pop();
         }
         if (!cmp.empty())
             return false;
         return true;        
     }
 };
+
