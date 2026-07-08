1178bee2cc23ef808253059e860c371f26e13caa
Priyank Raj <25353498+BigUstad@users.noreply.github.com>
2026-07-08 02:21:33 +0000

    [LeetCode Sync] Runtime - 7 ms (62.80%), Memory - 14.6 MB (88.77%)

--- solutions/0387-first-unique-character-in-a-string/solution.cpp
+++ solutions/0387-first-unique-character-in-a-string/solution.cpp
@@ -1,45 +1,20 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        std::map<char,int> cm;
-        std::set<char, std::less<char>> cs;
-        for ( size_t i=0; i < s.length(); ++i )
-        {
-            char c = s.at(i);
-            auto cmItr = cm.find(c);
-            if ( cmItr != cm.end() )
-            {
-                
-                cmItr->second++;
-            }
-            else
-            {
-                cm[c] = 1;
-            }
+        // std::unordered_map<char, int> repeats_table;
+        std::array<int, 26> char_table;
+        for (auto& c: s) {
+            // repeats_table[c]++;
+            char_table[c]++;
         }
-        
-        size_t leastIndex = -1;
-        for ( auto itr = cm.begin(); itr != cm.end(); ++itr)
-        {
-            if ( itr->second == 1 )
-            {
-                std::cout << " Single " << itr->first << std::endl;
-                cs.insert(itr->first);                
+        int i = 0;
+        for (auto&c: s) {
+            // if (repeats_table[c] == 1) {
+            if (char_table[c] == 1) {
+                return i;
             }
+            ++i;
         }
-
-        for ( auto itr = cs.begin(); itr != cs.end(); ++itr )
-        {
-            size_t pos = s.find(*itr);
-            if ( pos == std::string::npos )
-                continue;
-            if ( leastIndex == -1 )
-                leastIndex = pos;
-            else if ( pos < leastIndex )
-            {
-                leastIndex = pos;
-            }
-        }
-        return leastIndex;
+        return -1;
     }
 };
