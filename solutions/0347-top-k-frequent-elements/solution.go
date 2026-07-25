e6d306582f265838ff145235c4e0ec103d2b2c86
Priyank Raj <25353498+BigUstad@users.noreply.github.com>
2026-07-25 06:27:27 +0000

    [LeetCode Sync] Runtime - 3 ms (29.66%), Memory - 8.4 MB (8.27%)

--- solutions/0347-top-k-frequent-elements/solution.go
+++ solutions/0347-top-k-frequent-elements/solution.go
@@ -1,47 +1,25 @@
-import (
-	"fmt"
-
-	"github.com/emirpasic/gods/utils"
-	pq "github.com/emirpasic/gods/queues/priorityqueue"
-)
-
-
-type ElePair struct {
-    Ele int
-    Count int
-}
-// "-" descending order for min-heap
-// Normal ascending order for max-heap
-func pairComparator(i, j interface{}) int {
-    countI := i.(ElePair).Count
-    countJ := j.(ElePair).Count
-    return utils.IntComparator(countI, countJ) 
-}
-
 func topKFrequent(nums []int, k int) []int {
-    if len(nums) == k {
-        return nums
-    }
-    m := make(map[int]int)
-    for _, n := range nums {
-        if v, exists := m[n]; exists {
-            m[n] = v + 1
-        } else {
-            m[n] = 1
-        }
-    }
-
-    mh := pq.NewWith(pairComparator)
-    for ele, v := range m {
-        mh.Enqueue(ElePair {Ele: ele, Count: v})
-        if mh.Size() > k {
-            mh.Dequeue()
-        }
-    }
-    var vRet []int
-    for !mh.Empty() {
-		vEle, _ := mh.Dequeue()
-		vRet = append(vRet, vEle.(ElePair).Ele)
+	numMap := make(map[int]int)
+	n := len(nums)
+	for _, i := range nums {
+		numMap[i]++
 	}
-    return vRet
+	numBuckets := make([][]int,n+1)
+	// Bucket indexed
+	for k, v := range numMap {
+		numBuckets[v] = append(numBuckets[v], k)
+	}
+	var topk []int
+	for i := n; i > 0; i-- {
+		if len(numBuckets[i]) == 0 {
+			continue
+		}
+		if len(topk) >= k {
+			break
+		}
+		topk = append(topk, numBuckets[i]...)
+	}
+	// fmt.Println(len(topk))
+	// fmt.Println(tillk)
+	return topk[:k]
 }
