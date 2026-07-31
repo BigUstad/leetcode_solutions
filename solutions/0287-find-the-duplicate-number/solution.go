df7703709695a41852751410f2c3b5d36b9c7842
Priyank Raj <25353498+BigUstad@users.noreply.github.com>
2026-07-31 01:35:28 +0000

    [LeetCode Sync] Runtime - 31 ms (10.47%), Memory - 10.2 MB (40.76%)

---
+++ solutions/0287-find-the-duplicate-number/solution.go
@@ -0,0 +1,11 @@
+func findDuplicate(nums []int) int {
+    slices.Sort(nums)
+    l := len(nums) - 1
+    for i, n := range nums {
+        if i == l { break }
+        if n == nums[i+1] {
+            return n
+        }
+    }
+    return 0
+}
