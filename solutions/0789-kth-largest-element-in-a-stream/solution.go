5f981cf5a076ac8dd0cfcf519f69c8357d883e63
Priyank Raj <25353498+BigUstad@users.noreply.github.com>
2026-08-02 17:36:30 +0000

    [LeetCode Sync] Runtime - 7 ms (83.79%), Memory - 10.5 MB (21.74%)

---
+++ solutions/0789-kth-largest-element-in-a-stream/solution.go
@@ -0,0 +1,44 @@
+import (
+  pq "github.com/emirpasic/gods/queues/priorityqueue"
+  "github.com/emirpasic/gods/utils"
+)
+
+type KthLargest struct {
+    queue *pq.Queue
+    qK int
+}
+
+
+func Constructor(k int, nums []int) KthLargest {
+    var kth KthLargest
+    kth.queue = pq.NewWith(utils.IntComparator)
+    kth.qK = k
+    for _, n := range nums {
+        kth.Add(n)
+    }
+    return kth
+}
+
+
+func (this *KthLargest) Add(val int) int {
+    if this.queue.Size() == this.qK {
+        topEle, _ := this.queue.Peek()
+        top := topEle.(int)
+        // Not in top k
+        if val <= top { return top }
+        // Pop the least value, automatic heapify after add
+        _, _ = this.queue.Dequeue()
+    }
+    this.queue.Enqueue(val)
+
+    kthEle, _ := this.queue.Peek()
+    return kthEle.(int)
+}
+
+
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * obj := Constructor(k, nums);
+ * param_1 := obj.Add(val);
+ */
