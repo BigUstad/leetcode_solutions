a178863ad39bc1ca587ee2d1a0e06f447b5714dc
Priyank Raj <25353498+BigUstad@users.noreply.github.com>
2026-09-03 13:46:46 +0000

    [LeetCode Sync] Runtime - 126 ms (13.41%), Memory - 8.5 MB (29.31%)

--- solutions/0003-longest-substring-without-repeating-characters/solution.go
+++ solutions/0003-longest-substring-without-repeating-characters/solution.go
@@ -1,24 +1,16 @@
 func lengthOfLongestSubstring(s string) int {
-	l := len(s)
-	if l == 0 { return 0 }
-	r := []rune(s)
-	// First Sliding window length
-	m := make(map[rune]int)
-	max := 0
-	// fmt.Println(string(r[0:i]))
-	// Search for a new window if it exists
-	start := 0
-	for j := 0; j < l; j++ {
-		if x, ok := m[r[j]]; ok {
-			for start <= x {
-				delete(m, r[start])
-				start++
-			}			
-		}
-		m[r[j]] = j
-		if max < len(m) { max = len(m) }
-		// fmt.Println(m)
-		// fmt.Println(string(r[start:j+1]))
-	}
-	return max 
+    charSet := make(map[byte]bool)
+    l, res := 0, 0
+
+    for r := 0; r < len(s); r++ {
+        for charSet[s[r]] {
+            delete(charSet, s[l])
+            l++
+        }
+        charSet[s[r]] = true
+        if r - l + 1 > res {
+            res = r - l + 1
+        }
+    }
+    return res
 }
