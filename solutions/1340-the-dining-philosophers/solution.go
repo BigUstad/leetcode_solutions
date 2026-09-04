cdcdeaa3a5bd7eaa42715f1cfddc8e542b603883
Priyank Raj <25353498+BigUstad@users.noreply.github.com>
2026-09-04 23:40:35 +0000

    [LeetCode Sync] Runtime - 15 ms (62.89%), Memory - 5.8 MB (6.19%)

--- solutions/1340-the-dining-philosophers/solution.go
+++ solutions/1340-the-dining-philosophers/solution.go
@@ -1,8 +1,106 @@
 type DiningPhilosophers struct {
+    philInit sync.Once
+}
+
+const (
+    Eat = iota
+    Done
+    GoAhead
+)
+type DiningRequest struct {
+    philosopher int
+    signal int
+}
+
+type Coordinator struct {
     forks [5]bool
-    fCond *sync.Cond
-    fMu sync.Mutex
+    queue *list.List
+    doneCh chan struct{}
+}
+
+// Single Public request channel where all philosophers send signals
+// Request to eat. Signal done eating.
+// 5 private response channels where the coordinator tells yes or no.
+var (
+    co *Coordinator
     fInit sync.Once
+    reqCh chan DiningRequest
+    respCh [5]chan int
+)
+
+// GetCoordinator is the global entry point for all goroutines
+func GetCoordinator() *Coordinator {
+    // sync.Once guarantees the inner function runs EXACTLY once,
+    // blocking all other goroutines until the initialization completes.
+    fInit.Do(func() {
+        // fmt.Println("GetCoordinator()")
+        co = &Coordinator{}
+        for i := range co.forks {
+            co.forks[i] = true
+            respCh[i] = make(chan int, 1)
+        }
+        reqCh = make(chan DiningRequest, 5)
+        co.queue = list.New()
+        co.doneCh = make(chan struct{})
+        // for i := 0; i < 5; i++ {
+        //     if i % 2 == 0 {
+        //         respCh[i] <- DoWait
+        //     } else {
+        //         respCh[i] <- Eat
+        //     }
+        // }
+        go co.processDiningRequest()
+    })
+
+    return co
+}
+// fork indices i, j
+func (c *Coordinator) processEat(req DiningRequest) {
+    i := req.philosopher
+    j := (i + 1) % 5
+    // fmt.Println(c.forks)
+    if !c.forks[i] || !c.forks[j] {
+        c.queue.PushBack(req)
+    } else {
+        c.forks[i] = false
+        c.forks[j] = false
+        respCh[i] <- GoAhead
+    }
+}
+func (c *Coordinator) processQueue() {
+    if c.queue.Len() == 0 {
+        return
+    }
+    req := c.queue.Front().Value.(DiningRequest)
+    c.processEat(req)
+    c.queue.Remove(c.queue.Front())
+}
+
+func (c *Coordinator) processDiningRequest() {
+    // fmt.Println("processDiningRequest()")
+    for {
+        select {
+            case request := <-reqCh:
+                i := request.philosopher
+                j := (i + 1) % 5
+
+                switch request.signal {
+                case Eat:
+                    c.processEat(request)
+                case Done:
+                    c.forks[i] = true
+                    c.forks[j] = true
+                    c.processQueue()
+                }
+            case <-c.doneCh:
+                return
+        }
+        // time.Sleep(1 * time.Microsecond)
+    }
+}
+
+func (c *Coordinator) Close() {
+    close(c.doneCh)
 }
 
 func (this *DiningPhilosophers) wantsToEat(
@@ -13,44 +111,34 @@ func (this *DiningPhilosophers) wantsToEat(
     putLeftFork func(),
     putRightFork func(),
 ) {
-    this.fInit.Do(func() {
-        for i := 0; i < len(this.forks); i++ {
-            this.forks[i] = true
-        }
-        this.fCond = sync.NewCond(&this.fMu)
+    this.philInit.Do(func() {
+        // fmt.Println("one Init")
+        GetCoordinator()
     })
     // The complete implementation of the algo:
-    i := philosopher
-    j := (i + 1) % 5
-    // Check if left & right forks are available.
-    // If not wait till they are available
-    this.fMu.Lock()
-    for !this.forks[i] || !this.forks[j] {
-        this.fCond.Wait()
+    //  No need to do it in a for loop as the caller takes care of it
+    // 1. Request on public channel
+    // 2. Wait for response on private channel
+    // 3. Tell done on public channel
+
+    // Requesting with philosopher id & with signal to wanting to eat
+    req := DiningRequest{philosopher: philosopher, signal: Eat}
+    // // fmt.Println(req)
+    reqCh <- req
+    // Waiting on private response channel for coordinator to respond
+    // If signal says wait, try again later
+    sig := <- respCh[philosopher]
+
+    if sig == GoAhead {
+        // Coordinator has marked the forks unavailable.
+        // Executing eat
+        pickLeftFork()
+        pickRightFork()
+        eat()
+        putLeftFork()
+        putRightFork()
     }
-    // fmt.Print(philosopher)
-    // fmt.Print(").")
-    // fmt.Println(this.forks)
-    // Mark as unavailable
-    this.forks[i] = false
-    this.forks[j] = false
-    // fmt.Print(philosopher)
-    // fmt.Print(").")
-    // fmt.Println(this.forks)
-    // No need to lock for eat
-    this.fMu.Unlock()
-    // Execute eat
-    pickLeftFork()
-    pickRightFork()
-    eat()
-    putRightFork()
-    putLeftFork()
-    // Lock to update forks status to available
-    this.fMu.Lock()
-    this.forks[i] = true
-    this.forks[j] = true
-    // Broadcast to say forks are available
-    this.fCond.Broadcast()
-    this.fMu.Unlock()
+    // Coordinator will mark forks unavailable
+    reqCh <- DiningRequest{philosopher: philosopher, signal: Done}
 }
 
